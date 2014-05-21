2789c79fabff962e38f5d87a48684355ff8d8613
Ethan Gould <ethan.g.907@gmail.com>
2014-05-21 19:30:49 -0400

    updated routes for relations table nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'dashboard#index'
   devise_for :users
   get 'accept_user_input' => 'dashboard#index'
   post 'submit_user_input' => 'dashboard#submit'
 
-  resources :foods
+  resources :foods do
+    resources :shopping_list_food_relations
+  end
+
   resources :shopping_lists
+
 end
