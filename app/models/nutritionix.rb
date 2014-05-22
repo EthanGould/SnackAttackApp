4819d929fcf0b39eba1d28aa81c950c35de50749
Ethan Gould <ethan.g.907@gmail.com>
2014-05-22 15:20:01 -0400

    put body in container
    * also increase food search results from API

--- app/models/nutritionix.rb
+++ app/models/nutritionix.rb
@@ -5,7 +5,7 @@ class Nutritionix
   def self.response(query)
     api_key = ENV["NUTRITIONIX_API_KEY"]
     api_id = ENV["APPLICATION_ID"]
-    api_url = "https://api.nutritionix.com/v1_1/search/#{query}?results=0:05&fields=item_name,brand_name,item_id,nf_calories,nf_carbohydrates,nf_protein,nf_carbs&appId=#{api_id}&appKey=#{api_key}"
+    api_url = "https://api.nutritionix.com/v1_1/search/#{query}?results=0:07&fields=item_name,brand_name,item_id,nf_calories,nf_carbohydrates,nf_protein,nf_carbs&appId=#{api_id}&appKey=#{api_key}"
     HTTParty.get(api_url)
   end
 end
