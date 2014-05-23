dfb2ef770ae83b838d4face8fc55a76cba8451b4
Ethan Gould <ethan.g.907@gmail.com>
2014-05-22 22:16:48 -0400

    DRY'd up, added comments, improved variable naming

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,9 +10,9 @@ class DashboardController < ApplicationController
     if params['user_input_form'].present? && params['user_input_form']['user_input'].present?
       #calls from the nutritionix model, returns a hash
       @api_response = Nutritionix.response(search_params['user_input'])
-      @foods_array = save_response_to_db(@api_response['hits'])
+      @foods = save_response_to_db(@api_response['hits'])
     else
-      @foods_array = []
+      @foods = []
     end
   end
 
@@ -22,10 +22,12 @@ class DashboardController < ApplicationController
     params.require(:user_input_form).permit(:user_input)
   end
 
-  def save_response_to_db(result_array)
-    result_array.map { |food_hash|
-      tmp_food = Food.find_or_initialize_by(api_response_id: food_hash['fields']['item_id'])
-      tmp_food.update_attributes(name: food_hash['fields']['item_name'], calories: food_hash['fields']['nf_calories'], protein: food_hash['fields']['nf_protein'])
+  def save_response_to_db(result)
+    result.map { |food|
+      # if food item isnt already in DB, an instance of it is saved.
+      # also updates attributes of food from the API
+      tmp_food = Food.find_or_initialize_by(api_response_id: food['fields']['item_id'])
+      tmp_food.update_attributes(name: food['fields']['item_name'], calories: food['fields']['nf_calories'], protein: food['fields']['nf_protein'])
       tmp_food
     }
   end
